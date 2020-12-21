ef0a57b5bfe37ea982c6f9d7025f824521262f0d
StarShadow <oparkerj@gmail.com>
2020-12-20 23:00:25 -0700

    Cleanup

--- AdventOfCode2020/Puzzles/Day21.cs
+++ AdventOfCode2020/Puzzles/Day21.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode2020.Puzzles
 {
     public class Day21 : Puzzle
     {
-        public List<Food> Foods = new();
+        public readonly List<(List<string> Ingredients, List<string> Allergens)> Foods = new();
 
         public Day21()
         {
@@ -19,21 +19,9 @@ namespace AdventOfCode2020.Puzzles
         public void ReadInput()
         {
             var foods = Input.Extract<(List<string>, List<string>)>("(?:(\\w+) )+\\(contains (?:(\\w+)\\W+)+");
-            foreach (var (i, a) in foods)
+            foreach (var food in foods)
             {
-                Foods.Add(new Food(i, a));
-            }
-        }
-
-        public class Food
-        {
-            public readonly List<string> Ingredients;
-            public readonly List<string> Allergens;
-
-            public Food(List<string> ingredients, List<string> allergens)
-            {
-                Ingredients = ingredients;
-                Allergens = allergens;
+                Foods.Add(food);
             }
         }
 
@@ -46,31 +34,11 @@ namespace AdventOfCode2020.Puzzles
         {
             return Foods.Select(food => food.Allergens).Flatten();
         }
-        
-        public override void PartOne()
-        {
-            var possible = new Dictionary<string, List<string>>();
-            var all = AllIngredients().Distinct().ToArray();
-            foreach (var ing in all)
-            {
-                possible[ing] = AllAllergens().ToList();
-            }
-            foreach (var food in Foods)
-            {
-                foreach (var ing in all.Where(s => !food.Ingredients.Contains(s)))
-                {
-                    possible[ing].RemoveAll(s => food.Allergens.Contains(s));
-                }
-            }
 
-            var result = AllIngredients().Count(s => possible[s].Count == 0);
-            WriteLn(result);
-        }
-
-        public override void PartTwo()
+        public Dictionary<string, HashSet<string>> GetPossibleAllergens()
         {
             var possible = new Dictionary<string, HashSet<string>>();
-            var all = AllIngredients().ToArray();
+            var all = AllIngredients().Distinct().ToArray();
             foreach (var ing in all)
             {
                 possible[ing] = AllAllergens().ToHashSet();
@@ -82,11 +50,23 @@ namespace AdventOfCode2020.Puzzles
                     possible[ing].RemoveWhere(s => food.Allergens.Contains(s));
                 }
             }
+            return possible;
+        }
+        
+        public override void PartOne()
+        {
+            var possible = GetPossibleAllergens();
+            var result = AllIngredients().Count(s => possible[s].Count == 0);
+            WriteLn(result);
+        }
+
+        public override void PartTwo()
+        {
+            var possible = GetPossibleAllergens();
             foreach (var ing in AllIngredients().Distinct().Where(s => possible[s].Count == 0))
             {
                 possible.Remove(ing);
             }
-            
             possible.MakeSingles();
             var ingredients = possible.OrderBy(pair => pair.Value.First()).Select(pair => pair.Key);
             WriteLn(string.Join(',', ingredients));
