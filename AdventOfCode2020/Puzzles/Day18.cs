aa75c6d468cccaefbf6c936b65597e615297ae81
StarShadow <oparkerj@gmail.com>
2020-12-22 02:50:32 -0700

    Replace Day 18 with custom expression reader

--- AdventOfCode2020/Puzzles/Day18.cs
+++ AdventOfCode2020/Puzzles/Day18.cs
@@ -1,8 +1,6 @@
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using AdventToolkit;
-using AdventToolkit.Extensions;
+using AdventToolkit.Data;
 
 namespace AdventOfCode2020.Puzzles
 {
@@ -13,79 +11,23 @@ namespace AdventOfCode2020.Puzzles
             Part = 2;
         }
 
-        public IEnumerable<string> ExpressionSplit(string s)
-        {
-            var b = new StringBuilder();
-            for (var i = 0; i < s.Length; i++)
-            {
-                if (s[i] == ' ')
-                {
-                    if (b.Length > 0) yield return b.ToString();
-                    b.Length = 0;
-                }
-                else if (s[i] == '(')
-                {
-                    var end = s.GetEndParen(i);
-                    yield return s[(i + 1)..end];
-                    i = end;
-                    b.Length = 0;
-                }
-                else b.Append(s[i]);
-            }
-            if (b.Length > 0) yield return b.ToString();
-        }
-
-        public long Eval(string s)
-        {
-            var parts = ExpressionSplit(s).ToArray();
-            if (parts.Length == 1)
-            {
-                if (!parts[0].Contains(' ')) return long.Parse(parts[0]);
-                return Eval(parts[0]);
-            }
-            var current = Eval(parts[0]);
-            for (var i = 1; i < parts.Length; i += 2)
-            {
-                if (parts[i] == "+") current += Eval(parts[i + 1]);
-                else if (parts[i] == "*") current *= Eval(parts[i + 1]);
-            }
-            return current;
-        }
-
         public override void PartOne()
         {
-            var sum = Input.Select(Eval).Sum();
+            var reader = Expression<long>.Reader()
+                .ForConstants()
+                .AddBinaryOp("+", (a, b) => a + b, 1)
+                .AddBinaryOp("*", (a, b) => a * b, 1);
+            var sum = Input.Select(reader.GetResult).Sum();
             WriteLn(sum);
-        }
-        
-        public long Eval2(string s)
-        {
-            var parts = ExpressionSplit(s).ToArray();
-            if (parts.Length == 1)
-            {
-                if (!parts[0].Contains(' ')) return long.Parse(parts[0]);
-                return Eval(parts[0]);
-            }
-
-            while (parts.Search("+", out var i))
-            {
-                parts = parts[..(i - 1)].Append((Eval2(parts[i - 1]) + Eval2(parts[i + 1])).ToString())
-                    .Concat(parts[(i + 2)..])
-                    .ToArray();
-            }
-            while (parts.Search("*", out var i))
-            {
-                parts = parts[..(i - 1)].Append((Eval2(parts[i - 1]) * Eval2(parts[i + 1])).ToString())
-                    .Concat(parts[(i + 2)..])
-                    .ToArray();
-            }
-
-            return long.Parse(parts[0]);
         }
 
         public override void PartTwo()
         {
-            var sum = Input.Select(Eval2).Sum();
+            var reader = Expression<long>.Reader()
+                .ForConstants()
+                .AddBinaryOp("+", (a, b) => a + b, 2)
+                .AddBinaryOp("*", (a, b) => a * b, 1);
+            var sum = Input.Select(reader.GetResult).Sum();
             WriteLn(sum);
         }
     }
