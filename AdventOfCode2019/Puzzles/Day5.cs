71b077af5be81610d057bb1536673dd21f2ea4b2
StarShadow <oparkerj@gmail.com>
2021-08-08 03:30:22 -0700

    2019 Day 5

---
+++ AdventOfCode2019/Puzzles/Day5.cs
@@ -0,0 +1,32 @@
+using AdventToolkit;
+
+namespace AdventOfCode2019.Puzzles
+{
+    public class Day5 : Puzzle
+    {
+        public Day5()
+        {
+            Part = 2;
+        }
+
+        public override void PartOne()
+        {
+            var c = Computer.From(InputLine);
+            var data = new DataLink();
+            c.LineIn = data.Output;
+            c.LineOut = Computer.ConsoleOutput();
+            data.Insert(1);
+            c.Execute();
+        }
+
+        public override void PartTwo()
+        {
+            var c = Computer.From(InputLine);
+            var data = new DataLink();
+            c.LineIn = data.Output;
+            c.LineOut = Computer.ConsoleOutput();
+            data.Insert(5);
+            c.Execute();
+        }
+    }
+}
