ec1f21d883054b09501e2a3018daaa904c1dc906
StarShadow <oparkerj@gmail.com>
2021-08-09 00:33:26 -0700

    2019 Day 9

---
+++ AdventOfCode2019/Puzzles/Day9.cs
@@ -0,0 +1,15 @@
+using AdventToolkit;
+
+namespace AdventOfCode2019.Puzzles
+{
+    public class Day9 : Puzzle
+    {
+        public override void PartOne()
+        {
+            var c = Computer.From(InputLine);
+            c.LineIn = Computer.ConsoleReader();
+            c.LineOut = Computer.ConsoleOutput();
+            c.Execute();
+        }
+    }
+}
