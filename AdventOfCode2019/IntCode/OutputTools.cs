b0cd8918a846f2844d2d4396f0053769a99d51bc
StarShadow <oparkerj@gmail.com>
2021-08-19 03:00:42 -0700

    Add OutputTools

---
+++ AdventOfCode2019/IntCode/OutputTools.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+namespace AdventOfCode2019.IntCode
+{
+    public class OutputTools
+    {
+        private Computer Source;
+
+        public OutputTools(Computer source)
+        {
+            Source = source;
+        }
+
+        public IEnumerable<long> TakeCount(int amount)
+        {
+            for (var i = 0; i < amount; i++)
+            {
+                yield return Source.NextOutput();
+            }
+        }
+        
+        public IEnumerable<int> TakeCountInt(int amount)
+        {
+            for (var i = 0; i < amount; i++)
+            {
+                yield return Source.NextInt();
+            }
+        }
+        
+        public IEnumerable<bool> TakeCountBool(int amount)
+        {
+            for (var i = 0; i < amount; i++)
+            {
+                yield return Source.NextBool();
+            }
+        }
+    }
+}
