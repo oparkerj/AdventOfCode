68fec0ed26adc275cce484960ef796eb1bb62b81
StarShadow <oparkerj@gmail.com>
2021-08-21 20:35:57 -0700

    2019 Day 25

--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2019.Puzzles;
 using AdventToolkit;
 
-Puzzle.Run<Day24>();
+Puzzle.Run<Day25>();
---
+++ AdventOfCode2019/Puzzles/Day25.cs
@@ -0,0 +1,66 @@
+using AdventOfCode2019.IntCode;
+using AdventToolkit;
+using MoreLinq;
+
+namespace AdventOfCode2019.Puzzles
+{
+    public class Day25 : Puzzle
+    {
+        public override void PartOne()
+        {
+            var c = Computer.From(InputLine);
+            var data = new DataLink(c);
+            c.LineOut = Computer.AsciiOutput();
+
+            var setup =
+@"south
+take fixed point
+north
+west
+west
+west
+take hologram
+east
+east
+east
+north
+take candy cane
+west
+take antenna
+south
+take whirled peas
+north
+west
+take shell
+east
+east
+north
+north
+take polygon
+south
+west
+take fuel cell
+west
+";
+            var options = new[] {"fixed point", "hologram", "candy cane", "antenna", "whirled peas", "shell", "polygon", "fuel cell"};
+            
+            data.InsertAscii(setup);
+            options.ForEach(s => data.InsertAscii($"drop {s}\n"));
+            
+            foreach (var subset in options.Subsets())
+            {
+                foreach (var item in subset)
+                {
+                    data.InsertAscii($"take {item}\n");
+                }
+                data.InsertAscii("west\n");
+                foreach (var item in subset)
+                {
+                    data.InsertAscii($"drop {item}\n");
+                }
+            }
+            
+            c.Execute();
+        }
+    }
+}
