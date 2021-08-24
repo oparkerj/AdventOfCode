9cb3d674f4c8cf8dfdfd49d57d2e3622bae7b8c7
Parker <oparkerj@gmail.com>
2021-08-24 12:14:30 -0700

    Push files

---
+++ AdventOfCode2018/Puzzles/Day6.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventToolkit;
+using AdventToolkit.Extensions;
+using AdventToolkit.Utilities;
+using MoreLinq.Extensions;
+
+namespace AdventOfCode2018.Puzzles
+{
+    public class Day6 : Puzzle
+    {
+        public IEnumerable<Pos> GetPoints()
+        {
+            return Input.Select(Pos.Parse);
+        }
+
+        public KeyValuePair<int, Pos> Closest(Pos pos, IList<Pos> points)
+        {
+            return points.Index().SelectMin(pair => pos.MDist(pair.Value));
+        }
+
+        public override void PartOne()
+        {
+            var points = GetPoints().ToList();
+            var area = new Grid<int>();
+            foreach (var (i, point) in points.Index())
+            {
+                area[point] = i;
+            }
+            foreach (var pos in area.Bounds.Positions())
+            {
+                area[pos] = Closest(pos, points).Key;
+            }
+            var infinite = Enumerable.ToHashSet(area.Bounds.GetAllSides().Select(pos => area[pos]).Distinct());
+            var result = area.Values.Where(i => !infinite.Contains(i)).Frequencies().Max(pair => pair.Value);
+            WriteLn(result);
+            // TODO fix bug with this
+        }
+    }
+}
