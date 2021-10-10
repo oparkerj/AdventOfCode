fc57cd26a96b6f3dcde51a87cc9e92a3edbb2561
StarShadow <oparkerj@gmail.com>
2021-10-09 19:13:47 -0700

    Begin Day22

---
+++ AdventOfCode2018/Puzzles/Day22.cs
@@ -0,0 +1,16 @@
+using AdventToolkit;
+
+namespace AdventOfCode2018.Puzzles
+{
+    public class Day22 : Puzzle
+    {
+        public const int Rocky = 0;
+        public const int Wet = 1;
+        public const int Narrow = 2;
+        
+        public override void PartOne()
+        {
+            //
+        }
+    }
+}
