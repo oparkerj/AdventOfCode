4400bbe03e88f6a19255ac99d9f648c9d6420487
StarShadow <oparkerj@gmail.com>
2021-10-09 19:13:37 -0700

    Cleanup

--- AdventToolkit/Collections/Space/PortalGrid.cs
+++ AdventToolkit/Collections/Space/PortalGrid.cs
@@ -7,11 +7,11 @@ namespace AdventToolkit.Collections.Space
 {
     public class PortalGrid<T> : Grid<T>
     {
-        private Dictionary<Common.Pos, Dictionary<Side, Common.Pos>> _portals = new();
+        private Dictionary<Pos, Dictionary<Side, Pos>> _portals = new();
 
-        private Dictionary<Side, Common.Pos> GetPortals(Common.Pos p)
+        private Dictionary<Side, Pos> GetPortals(Pos p)
         {
-            return _portals.GetOrSetValue(p, () => new Dictionary<Side, Common.Pos>(1));
+            return _portals.GetOrSetValue(p, () => new Dictionary<Side, Pos>(1));
         }
 
         private IEnumerable<Side> Sides()
@@ -22,13 +22,13 @@ namespace AdventToolkit.Collections.Space
             yield return Side.Left;
         }
 
-        public override IEnumerable<Common.Pos> GetNeighbors(Common.Pos pos)
+        public override IEnumerable<Pos> GetNeighbors(Pos pos)
         {
             if (!_portals.TryGetValue(pos, out var sides)) return base.GetNeighbors(pos);
             return Sides().Except(sides.Keys).Select(side => pos.GetSide(side)).Concat(sides.Values);
         }
         
-        public void Connect(Common.Pos a, Side aSide, Common.Pos b, Side bSide)
+        public void Connect(Pos a, Side aSide, Pos b, Side bSide)
         {
             var aPortals = GetPortals(a);
             var bPortals = GetPortals(b);
@@ -37,28 +37,28 @@ namespace AdventToolkit.Collections.Space
         }
     }
 
-    public class PortalGrid<T, TTag> : AlignedSpace<(Common.Pos Pos, TTag Tag), T>
+    public class PortalGrid<T, TTag> : AlignedSpace<(Pos Pos, TTag Tag), T>
     {
-        private Dictionary<Common.Pos, Dictionary<Side, (Common.Pos Pos, TTag Tag)>> _portals = new();
+        private Dictionary<Pos, Dictionary<Side, (Pos Pos, TTag Tag)>> _portals = new();
         public PortalPass PassFunction;
 
-        public delegate bool PortalPass((Common.Pos Pos, TTag Tag) current, Common.Pos portal, ref TTag tag);
+        public delegate bool PortalPass((Pos Pos, TTag Tag) current, Pos portal, ref TTag tag);
 
-        public T this[Common.Pos pos]
+        public T this[Pos pos]
         {
             get => base[(pos, default)];
             set => base[(pos, default)] = value;
         }
 
-        public override T this[(Common.Pos Pos, TTag Tag) pos, bool sideEffects = true]
+        public override T this[(Pos Pos, TTag Tag) pos, bool sideEffects = true]
         {
             get => this[pos.Pos];
             set => this[pos.Pos] = value;
         }
 
-        private Dictionary<Side, (Common.Pos Pos, TTag Tag)> GetPortals(Common.Pos p)
+        private Dictionary<Side, (Pos Pos, TTag Tag)> GetPortals(Pos p)
         {
-            return _portals.GetOrSetValue(p, () => new Dictionary<Side, (Common.Pos Pos, TTag Tag)>(1));
+            return _portals.GetOrSetValue(p, () => new Dictionary<Side, (Pos Pos, TTag Tag)>(1));
         }
 
         private IEnumerable<Side> Sides()
@@ -69,7 +69,7 @@ namespace AdventToolkit.Collections.Space
             yield return Side.Left;
         }
 
-        public override IEnumerable<(Common.Pos Pos, TTag Tag)> GetNeighbors((Common.Pos Pos, TTag Tag) pos)
+        public override IEnumerable<(Pos Pos, TTag Tag)> GetNeighbors((Pos Pos, TTag Tag) pos)
         {
             if (!_portals.TryGetValue(pos.Pos, out var sides)) return pos.Pos.Adjacent().Select(p => (p, pos.Tag));
             return Sides().Except(sides.Keys).Select(side => (pos.Pos.GetSide(side), pos.Tag))
