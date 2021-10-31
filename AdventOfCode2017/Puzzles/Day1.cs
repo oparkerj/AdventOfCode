6ba1b157de151088099eb5df390c5897300dca0a
StarShadow <oparkerj@gmail.com>
2021-10-30 23:21:24 -0700

    Start 2017

---
+++ AdventOfCode2017/Puzzles/Day1.cs
@@ -0,0 +1,12 @@
+using AdventToolkit;
+
+namespace AdventOfCode2017.Puzzles
+{
+    public class Day1 : Puzzle
+    {
+        public override void PartOne()
+        {
+            //
+        }
+    }
+}
