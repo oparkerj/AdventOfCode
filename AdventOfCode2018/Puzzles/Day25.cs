c2c30365f18ab11c8fbc0b61e34522f841e5a7b4
StarShadow <oparkerj@gmail.com>
2021-10-30 22:42:45 -0700

    2018 Day 25

---
+++ AdventOfCode2018/Puzzles/Day25.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+using AdventToolkit;
+using AdventToolkit.Common;
+using AdventToolkit.Extensions;
+
+namespace AdventOfCode2018.Puzzles
+{
+    public class Day25 : Puzzle
+    {
+        public List<Pos4D> ReadPoints() => Input.Select(Pos4D.Parse).ToList();
+        
+        public override void PartOne()
+        {
+            var count = ReadPoints().GroupPairs((a, b) => a.MDist(b) <= 3).Count();
+            WriteLn(count);
+        }
+    }
+}
