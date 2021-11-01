fdb21267356d496c6bed8caa22c8aa6048a6e114
StarShadow <oparkerj@gmail.com>
2021-10-31 23:58:00 -0700

    2017 Day 4

---
+++ AdventOfCode2017/Puzzles/Day4.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+using AdventToolkit;
+using AdventToolkit.Extensions;
+
+namespace AdventOfCode2017.Puzzles
+{
+    public class Day4 : Puzzle
+    {
+        public Day4()
+        {
+            Part = 2;
+        }
+
+        public override void PartOne()
+        {
+            var result = Input.Count(s => s.Split(' ').FirstRepeat() == null);
+            WriteLn(result);
+        }
+
+        public override void PartTwo()
+        {
+            var result = Input.Count(s => s.Split(' ').Select(w => w.Sorted().Str()).FirstRepeat() == null);
+            WriteLn(result);
+        }
+    }
+}
