d8c00612c9d73a8c3511632c2bd6c4aa3df0f931
Parker <oparkerj@gmail.com>
2021-11-02 09:37:20 -0700

    2017 Day 5

---
+++ AdventOfCode2017/Puzzles/Day5.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using AdventToolkit;
+using AdventToolkit.Extensions;
+
+namespace AdventOfCode2017.Puzzles
+{
+    public class Day5 : Puzzle
+    {
+        public Day5()
+        {
+            Part = 2;
+        }
+
+        public override void PartOne()
+        {
+            var jumps = Input.Ints().ToArray();
+            var ptr = 0;
+            var count = 0;
+            while (ptr >= 0 && ptr < jumps.Length)
+            {
+                ptr += jumps[ptr]++;
+                count++;
+            }
+            WriteLn(count);
+        }
+
+        public override void PartTwo()
+        {
+            var jumps = Input.Ints().ToArray();
+            var ptr = 0;
+            var count = 0;
+            while (ptr >= 0 && ptr < jumps.Length)
+            {
+                ptr += jumps[ptr] >= 3 ? jumps[ptr]-- : jumps[ptr]++;
+                count++;
+            }
+            WriteLn(count);
+        }
+    }
+}
