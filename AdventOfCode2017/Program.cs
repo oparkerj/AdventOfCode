96aacb7549c8bd271144c7eb7585da1c08a46634
Parker <oparkerj@gmail.com>
2021-11-02 13:46:48 -0700

    Progress

--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2017.Puzzles;
 using AdventToolkit;
 
-Puzzle.Run<Day6>();
+Puzzle.Run<Day7>();
---
+++ AdventOfCode2017/Puzzles/Day7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventToolkit;
+using AdventToolkit.Collections;
+using AdventToolkit.Extensions;
+using RegExtract;
+
+namespace AdventOfCode2017.Puzzles
+{
+    public class Day7 : Puzzle
+    {
+        public Day7()
+        {
+            Part = 2;
+        }
+
+        public override void PartOne()
+        {
+            var tree = Input.ToWeightedTree(@"(?<Value>.+) \((?<Amount>\d+)\)(?: -> (?:(?<Children>\w+)(?:, )?)+)?");
+            var vertex = tree.DetermineRoot();
+            WriteLn(vertex.Value);
+        }
+
+        public override void PartTwo()
+        {
+            var tree = Input.ToDigraph(s => s.Extract<(string Name, int Weight, List<string> Children)>(@"(\w+) \((\d+)\)(?: -> (?:(\w+)(?:, )?)+)?"),
+                tuple => tuple.Name,
+                tuple => );
+            // var weightFunction = Data.Memoize<QuantityVertex<string>, long>(tree.TotalVertexWeight);
+            // var incorrect = tree.Bfs().First(v => !v.Neighbors.Select(weightFunction).AllEqual());
+            // var (min, max) = incorrect.Neighbors.Select(weightFunction).Frequencies().ExtentsBy(pair => pair.Value);
+            // WriteLn(max.Key);
+        }
+    }
+}
