23cf791379d14f2169823d75a3f10048893f1715
StarShadow <oparkerj@gmail.com>
2021-11-08 21:11:25 -0700

    Modify AstReader to not use recursion

---
+++ AdventToolkit/Extensions/CollectionExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace AdventToolkit.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static void Replace<T>(this Stack<T> stack, T t)
+        {
+            if (!stack.TryPop(out _)) throw new Exception("Stack is empty.");
+            stack.Push(t);
+        }
+    }
+}
