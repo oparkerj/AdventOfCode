46bbd08983c16a1c918fffd6898e7fc776a556f4
Parker <oparkerj@gmail.com>
2021-11-16 21:28:31 -0700

    Start solution

---
+++ AdventOfCode2017/Puzzles/Day24.cs
@@ -0,0 +1,17 @@
+﻿using AdventToolkit;
+
+namespace AdventOfCode2017.Puzzles
+{
+    public class Day24 : Puzzle
+    {
+        public Day24()
+        {
+            // Part = 2;
+        }
+
+        public override void PartOne()
+        {
+            //
+        }
+    }
+}
