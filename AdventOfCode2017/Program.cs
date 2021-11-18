acf2827b5a08b06d0d255cf70eba71ecf4224c5e
Parker <oparkerj@gmail.com>
2021-11-18 13:38:24 -0700

    2017 Day 25

--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2017.Puzzles;
 using AdventToolkit;
 
-Puzzle.Run<Day24>();
+Puzzle.Run<Day25>();
---
+++ AdventToolkit/Utilities/Automata/TuringMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventToolkit.Collections.Space;
+
+namespace AdventToolkit.Utilities.Automata
+{
+    public class TuringMachine<T> : StateMachine<T>
+    {
+        public readonly Dictionary<(int, T), (T, bool)> Update = new();
+
+        public readonly Line<T> Tape = new();
+
+        public int Reader = 0;
+        
+        public void RunSteps(int steps)
+        {
+            var tape = Tape;
+            var state = InitialState;
+            for (var i = 0; i < steps; i++)
+            {
+                var update = tape[Reader];
+                if (!Update.TryGetValue((state, update), out var action)) throw new Exception("No action for current state.");
+                tape[Reader] = action.Item1;
+                if (action.Item2) Reader++;
+                else Reader--;
+                if (!Step(state, update, out state)) throw new Exception("No target for current state.");
+            }
+        }
+    }
+}
