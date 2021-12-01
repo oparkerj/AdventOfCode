72a8b8039eb1c94473a19fad4b45c92760757b53
StarShadow <oparkerj@gmail.com>
2021-11-30 22:14:54 -0700

    2021 Day 1

---
+++ AdventOfCode2021/Puzzles/Day1.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using AdventToolkit;
+using AdventToolkit.Extensions;
+using MoreLinq;
+
+namespace AdventOfCode2021.Puzzles
+{
+    public class Day1 : Puzzle
+    {
+        public Day1()
+        {
+            Part = 2;
+        }
+
+        public override void PartOne()
+        {
+            var count = Input.Ints().Pairwise((a, b) => b > a).Count(true);
+            WriteLn(count);
+        }
+
+        public override void PartTwo()
+        {
+            var count = Input.Ints().Window(3).Select(w => w.Sum()).Pairwise((a, b) => b > a).Count(true);
+            WriteLn(count);
+        }
+    }
+}
