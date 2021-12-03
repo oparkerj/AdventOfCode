9a068e64afde126a8c8a1d99ac645883e2918fc0
StarShadow <oparkerj@gmail.com>
2021-12-03 00:18:47 -0700

    2021 Day 3

---
+++ AdventOfCode2021/Puzzles/Day3.cs
@@ -0,0 +1,60 @@
+using System.Linq;
+using AdventToolkit;
+using AdventToolkit.Collections;
+using AdventToolkit.Extensions;
+
+namespace AdventOfCode2021.Puzzles
+{
+    public class Day3 : Puzzle
+    {
+        public Day3()
+        {
+            Part = 2;
+        }
+
+        public override void PartOne()
+        {
+            var len = Input.Length;
+            var counts = new DefaultDict<int, int>();
+            foreach (var s in Input)
+            {
+                for (var i = 0; i < s.Length; i++)
+                {
+                    counts[i] += s[i].AsInt();
+                }
+            }
+
+            var result = 0;
+            foreach (var (i, count) in counts)
+            {
+                result |= (count > len / 2).AsInt() << (11 - i);
+            }
+            WriteLn(result * (~result & 0xFFF));
+        }
+
+        public override void PartTwo()
+        {
+            var gen = Input.ToList();
+            var i = 0;
+            while (gen.Count > 1)
+            {
+                var ones = gen.Select(s => s[i].AsInt()).Sum();
+                var most = !new Interval(gen.Count).InLowerHalf(ones);
+                gen.RemoveAll(s => s[i] == '1' != most);
+                i++;
+            }
+            
+            var o2 = Input.ToList();
+            i = 0;
+            while (o2.Count > 1)
+            {
+                var ones = o2.Select(s => s[i].AsInt()).Sum();
+                var least = new Interval(o2.Count).InLowerHalf(ones);
+                o2.RemoveAll(s => s[i] == '1' != least);
+                i++;
+            }
+            
+            WriteLn(gen[0].BinaryInt() * o2[0].BinaryInt());
+        }
+    }
+}
