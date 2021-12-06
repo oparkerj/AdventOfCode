b8f4ebcac5243703c8b8a958badb8bf3599f75b6
Parker <oparkerj@gmail.com>
2021-12-05 22:56:43 -0700

    2021 Day 6

---
+++ AdventToolkit/Extensions/RectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using AdventToolkit.Collections;
+using AdventToolkit.Common;
+
+namespace AdventToolkit.Extensions
+{
+    public static class RectExtensions
+    {
+        public static Rect ToRect(this (Pos A, Pos B) corners)
+        {
+            return new Rect(corners.A, corners.B);
+        }
+    }
+}
