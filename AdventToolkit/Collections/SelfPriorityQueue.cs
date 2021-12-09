52720fd168d165c4e04c418ec7841538bff42031
StarShadow <oparkerj@gmail.com>
2021-12-09 01:16:28 -0700

    Missed one char

--- AdventToolkit/Collections/SelfPriorityQueue.cs
+++ AdventToolkit/Collections/SelfPriorityQueue.cs
@@ -7,7 +7,7 @@ public class SelfPriorityQueue<TKey> : PriorityQueue<TKey, TKey>
     public SelfPriorityQueue() { }
     public SelfPriorityQueue(IComparer<TKey> comparer) : base(comparer) { }
     public SelfPriorityQueue(IEnumerable<(TKey Element, TKey Priority)> items) : base(items) { }
-    public SelfPriorityQueue(IEnumerable<(TKey Element, TKey Priority)> items, IComparer<TKey>? comparer) : base(items, comparer) { }
+    public SelfPriorityQueue(IEnumerable<(TKey Element, TKey Priority)> items, IComparer<TKey> comparer) : base(items, comparer) { }
     public SelfPriorityQueue(int initialCapacity) : base(initialCapacity) { }
     public SelfPriorityQueue(int initialCapacity, IComparer<TKey> comparer) : base(initialCapacity, comparer) { }
 
