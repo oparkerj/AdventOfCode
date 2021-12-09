fd7c262fde55717f156a73cde88ba3e984530abb
StarShadow <oparkerj@gmail.com>
2021-12-09 01:15:42 -0700

    Upgrade to .NET 6 / C# 10

---
+++ AdventToolkit/Collections/SelfPriorityQueue.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace AdventToolkit.Collections;
+
+public class SelfPriorityQueue<TKey> : PriorityQueue<TKey, TKey>
+{
+    public SelfPriorityQueue() { }
+    public SelfPriorityQueue(IComparer<TKey> comparer) : base(comparer) { }
+    public SelfPriorityQueue(IEnumerable<(TKey Element, TKey Priority)> items) : base(items) { }
+    public SelfPriorityQueue(IEnumerable<(TKey Element, TKey Priority)> items, IComparer<TKey>? comparer) : base(items, comparer) { }
+    public SelfPriorityQueue(int initialCapacity) : base(initialCapacity) { }
+    public SelfPriorityQueue(int initialCapacity, IComparer<TKey> comparer) : base(initialCapacity, comparer) { }
+
+    public void Enqueue(TKey key) => Enqueue(key, key);
+}
