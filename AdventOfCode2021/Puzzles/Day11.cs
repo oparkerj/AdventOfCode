e863ac95dfb41be94c2151d93416c5eafafb8d79
StarShadow <oparkerj@gmail.com>
2021-12-10 16:34:47 -0700

    prepare for day 11

---
+++ AdventOfCode2021/Puzzles/Day11.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode2021.Puzzles;
+
+public class Day11 : Puzzle
+{
+    public Day11()
+    {
+        Part = 2;
+    }
+
+    public override void PartOne()
+    {
+        
+    }
+}
