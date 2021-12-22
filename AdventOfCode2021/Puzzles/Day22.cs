a2bf3b1f938684968c7fdd4249ba72d870e04bc8
StarShadow <oparkerj@gmail.com>
2021-12-22 16:59:49 -0700

    Cleanup

--- AdventOfCode2021/Puzzles/Day22.cs
+++ AdventOfCode2021/Puzzles/Day22.cs
@@ -17,7 +17,7 @@ public class Day22 : Puzzle
     
     public record Region(Interval X, Interval Y, Interval Z, bool On = false);
 
-    public Region Read(string line)
+    public static Region Read(string line)
     {
         var (a, b, c, d, e, f) = line.Extract<(int, int, int, int, int, int)>(@".+ x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)");
         return new Region(Interval.RangeInclusive(a, b), Interval.RangeInclusive(c, d), Interval.RangeInclusive(e, f), line.StartsWith("on"));
@@ -25,7 +25,7 @@ public class Day22 : Puzzle
 
     public override void PartOne()
     {
-        var blocks = new Blocks<bool>();
+        var cloud = new Cloud();
         var valid = Interval.RangeInclusive(-50, 50);
         
         foreach (var s in Input)
@@ -37,13 +37,13 @@ public class Day22 : Puzzle
                 {
                     foreach (var k in z.Overlap(valid))
                     {
-                        blocks[new Pos3D(i, j, k)] = on;
+                        cloud[new Pos3D(i, j, k)] = on;
                     }
                 }
             }
         }
         
-        WriteLn(blocks.CountValues(true));
+        WriteLn(cloud.Count);
     }
 
     public Region GetOverlap(Region first, Region second)
@@ -59,6 +59,9 @@ public class Day22 : Puzzle
         return (BigInteger) x.Length * y.Length * z.Length;
     }
 
+    // Get the number of squares that a region contributes to the "final" result.
+    // The contribution to the final result is how many units are not overridden by a future
+    // instruction.
     public BigInteger GetContribution(Region region, IEnumerable<Region> possibleOverlaps)
     {
         var volume = Size(region);
@@ -83,6 +86,6 @@ public class Day22 : Puzzle
             .WhereValue(r => r.On)
             .Select(pair => GetContribution(pair.Value, regions.Skip(pair.Key + 1)))
             .Aggregate(BigInteger.Add);
-        Clip(total);
+        WriteLn(total);
     }
 }
