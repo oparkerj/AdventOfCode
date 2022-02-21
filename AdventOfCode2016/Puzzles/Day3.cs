bbe1634d1c3f8dbce22e21c7918932b32bf0e793
Parker <oparkerj@gmail.com>
2022-02-21 12:21:02 -0700

    2016 Day 3

---
+++ AdventOfCode2016/Puzzles/Day3.cs
@@ -0,0 +1,35 @@
+﻿using AdventToolkit;
+using AdventToolkit.Extensions;
+using RegExtract;
+
+namespace AdventOfCode2016.Puzzles;
+
+public class Day3 : Puzzle
+{
+    public Day3()
+    {
+        Part = 2;
+    }
+
+    public override void PartOne()
+    {
+        var result = Input.Extract<Triangle>(@"\s*(\d+)\s+(\d+)\s+(\d+)\s*").Count(triangle => triangle.IsPossible);
+        Clip(result);
+    }
+    
+    public override void PartTwo()
+    {
+        var count = Input.Select(s => s.Spaced())
+            .Chunk(3)
+            .Select(s => Enumerable.Range(0, 3).Select(i => s.Select(r => r[i].AsInt()).ToArray(3)))
+            .Flatten()
+            .Select(ints => new Triangle(ints[0], ints[1], ints[2]))
+            .Count(triangle => triangle.IsPossible);
+        Clip(count);
+    }
+
+    public record Triangle(int A, int B, int C)
+    {
+        public bool IsPossible => A + B > C && B + C > A && A + C > B;
+    }
+}
