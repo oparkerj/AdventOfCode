3e74d53e6eeb2fba5c7c52224bba8bec5b8e61f9
Parker <oparkerj@gmail.com>
2022-03-28 13:09:44 -0700

    2016 Day 12

--- AdventOfCode2016/Program.cs
+++ AdventOfCode2016/Program.cs
@@ -2,4 +2,4 @@
 using AdventOfCode2016.Puzzles;
 using AdventToolkit;
 
-Puzzle.Run<Day10>();
+Puzzle.Run<Day12>();
---
+++ AdventOfCode2016/Puzzles/Day12.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+using AdventToolkit;
+using AdventToolkit.Extensions;
+
+namespace AdventOfCode2016.Puzzles;
+
+public class Day12 : Puzzle
+{
+    private BigInteger[] _regs;
+    private int _ptr;
+
+    public Day12()
+    {
+        _regs = new BigInteger[4];
+    }
+
+    public int Reg(char r) => r - 'a';
+
+    public BigInteger Val(string s) => char.IsLetter(s[0]) ? _regs[Reg(s[0])] : s.AsInt();
+
+    public void Execute(string inst)
+    {
+        var parts = inst.Split(' ');
+
+        if (parts[0] == "cpy") _regs[Reg(parts[2][0])] = Val(parts[1]);
+        else if (parts[0] == "inc") _regs[Reg(parts[1][0])]++;
+        else if (parts[0] == "dec") _regs[Reg(parts[1][0])]--;
+        else if (parts[0] == "jnz" && Val(parts[1]) != 0) _ptr += parts[2].AsInt() - 1;
+    }
+
+    public override void PartOne()
+    {
+        while (_ptr < Input.Length)
+        {
+            Execute(Input[_ptr]);
+            _ptr++;
+        }
+        
+        WriteLn(_regs[0]);
+    }
+
+    public override void PartTwo()
+    {
+        _regs[Reg('c')] = 1;
+        
+        PartOne();
+    }
+}
