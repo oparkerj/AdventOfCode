539e80eb6b29316b9d41bae3f3373a27859bd507
StarShadow <oparkerj@gmail.com>
2022-07-26 21:45:34 -0700

    2015 Day 14

---
+++ AdventOfCode2015/Puzzles/Day14.cs
@@ -0,0 +1,44 @@
+using AdventToolkit;
+using AdventToolkit.Common;
+using AdventToolkit.Extensions;
+using MoreLinq;
+using RegExtract;
+
+namespace AdventOfCode2015.Puzzles;
+
+public class Day14 : Puzzle
+{
+    public const int TimeLimit = 2503;
+
+    public record Reindeer(int Speed, int Time, int Rest)
+    {
+        public int ComputeDistance(int time)
+        {
+            var (chunks, offset) = Math.DivRem(time, Time + Rest);
+            return Speed * Time * chunks + Speed * Math.Min(offset, Time);
+        }
+    }
+    
+    public override void PartOne()
+    {
+        var result = Input.Extract<Reindeer>(Patterns.Int3).Select(r => r.ComputeDistance(TimeLimit)).Max();
+        WriteLn(result);
+    }
+
+    public override void PartTwo()
+    {
+        var reindeer = Input.Extract<Reindeer>(Patterns.Int3).ToList();
+        var points = new int[reindeer.Count];
+        foreach (var i in Enumerable.Range(1, TimeLimit))
+        {
+            var winners = reindeer.Index()
+                .MaxsBy(pair => pair.Value.ComputeDistance(i))
+                .Keys();
+            foreach (var winner in winners)
+            {
+                points[winner]++;
+            }
+        }
+        WriteLn(points.Max());
+    }
+}
