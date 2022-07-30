e6814a0e70a55e0014268abbc6a139bc1eea4052
StarShadow <oparkerj@gmail.com>
2022-07-29 18:11:39 -0700

    Cleanup

--- AdventToolkit/Common/Patterns.cs
+++ AdventToolkit/Common/Patterns.cs
@@ -1,16 +1,21 @@
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AdventToolkit.Common;
 
 public static class Patterns
 {
-    public static Regex Nd => new(@"[^0-9\-]");
+    public static Regex NonDigit => new(@"[^0-9\-]");
+
+    public static Regex Int => new(@"(-?\d+)");
+
+    public static Regex IntPattern(int amount) => new(string.Join($"{NonDigit}+", Enumerable.Repeat(Int.ToString(), amount)));
     
-    public static Regex Int2 => new(@$"(-?\d+){Nd}+(-?\d+)");
+    public static Regex Int2 => IntPattern(2);
     
-    public static Regex Int3 => new(@$"(-?\d+){Nd}+(-?\d+){Nd}+(-?\d+)");
+    public static Regex Int3 => IntPattern(3);
     
-    public static Regex Int4 => new(@$"(-?\d+){Nd}+(-?\d+){Nd}+(-?\d+){Nd}+(-?\d+)");
+    public static Regex Int4 => IntPattern(4);
     
-    public static Regex Int5 => new(@$"(-?\d+){Nd}+(-?\d+){Nd}+(-?\d+){Nd}+(-?\d+){Nd}+(-?\d+)");
+    public static Regex Int5 => IntPattern(5);
 }
