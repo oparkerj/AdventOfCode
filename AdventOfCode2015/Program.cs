290ab8b4b6588f564caf4c36510a3d297eca4a63
StarShadow <oparkerj@gmail.com>
2022-08-04 21:32:59 -0700

    2015 Day 23

--- AdventOfCode2015/Program.cs
+++ AdventOfCode2015/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2015.Puzzles;
 using AdventToolkit;
 
-Puzzle.Run<Day22>();
+Puzzle.Run<Day23>();
---
+++ AdventOfCode2015/Puzzles/Day23.cs
@@ -0,0 +1,69 @@
+using System.Numerics;
+using AdventToolkit;
+using AdventToolkit.Common;
+using AdventToolkit.Utilities;
+
+namespace AdventOfCode2015.Puzzles;
+
+public class Day23 : Puzzle
+{
+    public BigInteger[] Register = new BigInteger[2];
+    public int Ptr = 0;
+    
+    public override void PartOne()
+    {
+        var matcher = new StringMatcher();
+        matcher.AddPrefix<char>("hlf", @"hlf (.)", Half);
+        matcher.AddPrefix<char>("tpl", @"tpl (.)", Triple);
+        matcher.AddPrefix<char>("inc", @"inc (.)", Increment);
+        matcher.AddPrefix<int>("jmp", Patterns.Int.ToString(), Jump);
+        matcher.AddPrefix<char, int>("jie", @"jie (.), " + Patterns.Int, JumpIfEven);
+        matcher.AddPrefix<char, int>("jio", @"jio (.), " + Patterns.Int, JumpIfOne);
+
+        while (Ptr >= 0 && Ptr < Input.Length)
+        {
+            matcher.Handle(Input[Ptr]);
+            Ptr++;
+        }
+        
+        WriteLn(Register[Reg('b')]);
+    }
+
+    public override void PartTwo()
+    {
+        Register[Reg('a')] = 1;
+        PartOne();
+    }
+
+    public int Reg(char c) => c - 'a';
+
+    public void Half(char r)
+    {
+        Register[Reg(r)] /= 2;
+    }
+
+    public void Triple(char r)
+    {
+        Register[Reg(r)] *= 3;
+    }
+
+    public void Increment(char r)
+    {
+        Register[Reg(r)]++;
+    }
+
+    public void Jump(int offset)
+    {
+        Ptr += offset - 1;
+    }
+
+    public void JumpIfEven(char r, int offset)
+    {
+        if (Register[Reg(r)] % 2 == 0) Jump(offset);
+    }
+
+    public void JumpIfOne(char r, int offset)
+    {
+        if (Register[Reg(r)] == 1) Jump(offset);
+    }
+}
