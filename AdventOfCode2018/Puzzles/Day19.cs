102eb6dc885ff9097ff495cc4efe928390c32499
StarShadow <oparkerj@gmail.com>
2022-11-01 00:34:36 -0700

    Improve 2018 Day 19

--- AdventOfCode2018/Puzzles/Day19.cs
+++ AdventOfCode2018/Puzzles/Day19.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using AdventToolkit;
 using AdventToolkit.Collections;
 using AdventToolkit.Extensions;
+using AdventToolkit.Utilities.Computer;
 using RegExtract;
 
 namespace AdventOfCode2018.Puzzles;
@@ -60,5 +61,52 @@ public class Day19 : Puzzle
         var d = reg[2];
         var result = Interval.RangeInclusive(1, d).Where(i => d % i == 0).Sum();
         WriteLn(result);
+    }
+}
+
+public class Day19_2 : Puzzle<int>
+{
+    public Day19_2()
+    {
+        InputName = CopyInput<Day19>();
+        Part = 1;
+    }
+
+    public override int PartOne()
+    {
+        var day = new Day16_2();
+        var cpu = day.Cpu;
+        var builder = day.Builder;
+        // Now using 6 registers
+        cpu.Memory = new Registers<int>(6);
+        // Set up a pipeline that performs the register binding.
+        cpu.Pipeline = new RegisterBindPipeline
+        {
+            Bind = new IntBinder().Bind(cpu, InputLine[4..])
+        };
+        
+        builder.ParseAllInto(cpu, Input.Skip(1));
+        cpu.Execute();
+        return cpu.Memory[0];
+    }
+    
+    // Part two is the same as the original
+    public override int PartTwo()
+    {
+        var result = RunCapture<Day19>();
+        return result.AsInt();
+    }
+
+    class RegisterBindPipeline : StandardPipeline<int>
+    {
+        public Mem<int> Bind { get; set; }
+
+        public override bool Tick(Cpu<int> cpu)
+        {
+            Bind.Value = cpu.Pointer;
+            var result = base.Tick(cpu);
+            cpu.Pointer = Bind.Value + 1;
+            return result;
+        }
     }
 }
--- AdventToolkit/Utilities/Computer/StandardPipeline.cs
+++ AdventToolkit/Utilities/Computer/StandardPipeline.cs
@@ -3,19 +3,19 @@ namespace AdventToolkit.Utilities.Computer;
 // Runs the cpu by executing an instruction and then incrementing the pointer.
 public class StandardPipeline<TArch> : IPipeline<TArch>
 {
-    public bool Tick(Cpu<TArch> cpu)
+    public virtual bool Tick(Cpu<TArch> cpu)
     {
         var halt = cpu.InstructionSet.ExecuteNext(cpu);
         cpu.Pointer++;
         return halt;
     }
 
-    public void JumpRelative(Cpu<TArch> cpu, int offsetToNext)
+    public virtual void JumpRelative(Cpu<TArch> cpu, int offsetToNext)
     {
         cpu.Pointer += offsetToNext - 1;
     }
 
-    public void JumpTo(Cpu<TArch> cpu, int next)
+    public virtual void JumpTo(Cpu<TArch> cpu, int next)
     {
         cpu.Pointer = next - 1;
     }
