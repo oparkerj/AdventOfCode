b928191d469e9316fcd357d6fce5d2b94dcbe5b5
StarShadow <oparkerj@gmail.com>
2022-11-20 23:24:04 -0700

    Improve 2015 Day 19 Use new span pattern matching

--- AdventOfCode2015/Puzzles/Day19.cs
+++ AdventOfCode2015/Puzzles/Day19.cs
@@ -32,17 +32,18 @@ public class Day19 : Puzzle<int>
 
     public override int PartTwo()
     {
-        var target = AllGroups[1][0];
+        var target = AllGroups[1][0].AsSpan();
         var result = -1;
 
-        for (var i = 0; i < target.Length; i++)
+        for (var i = 0; i < target.Length - 1; i++)
         {
-            var c = target[i];
-            if (c == 'R' && i < target.Length - 1 && target[i + 1] == 'n') continue;
-            if (c == 'A' && i < target.Length - 1 && target[i + 1] == 'r') continue;
-            if (c == 'Y') result--;
-            else if (char.IsUpper(c)) result++;
+            var part = target.Slice(i, 2);
+            if (part is "Rn") continue;
+            if (part is "Ar") continue;
+            if (part[0] == 'Y') result--;
+            else if (char.IsUpper(part[0])) result++;
         }
+        if (char.IsUpper(target[^1])) result++;
         
         return result;
     }
