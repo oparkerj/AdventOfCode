ce72336be2d3106538307eeb5aa04bfc64b1e70c
Parker <oparkerj@gmail.com>
2023-12-11 22:47:35 -0700

    2023 Day 12 Part 1

---
+++ AdventOfCode2023/Puzzles/Day12.cs
@@ -0,0 +1,36 @@
+﻿using AdventToolkit;
+using AdventToolkit.Attributes;
+using AdventToolkit.Extensions;
+using MoreLinq;
+
+namespace AdventOfCode2023.Puzzles;
+
+public class Day12 : Puzzle<int>
+{
+    public override int PartOne()
+    {
+        var rows = Input.Select(s => s.SingleSplit(' ')).Select(tuple => (tuple.Left, tuple.Right.Csv().Ints().ToList()));
+        var realTotal = 0;
+        foreach (var (given, groups) in rows)
+        {
+            realTotal += CountPossible(given, groups);
+        }
+        return realTotal;
+        
+        int CountPossible(string given, List<int> groups)
+        {
+            var requiredMask = (uint) given.Select(c => c == '#' ? 1 : 0).BitsToInt();
+            var emptyMask = (uint) given.Select(c => c == '.' ? 0 : 1).BitsToInt();
+            var total = 0;
+            for (var i = 0U; i < (1 << 20); i++)
+            {
+                if ((i | requiredMask) != i || (i & emptyMask) != i) continue;
+                if (i.Bits().RunLengthEncode().WhereKey(true).Values().SequenceEqual(groups))
+                {
+                    total++;
+                }
+            }
+            return total;
+        }
+    }
+}
