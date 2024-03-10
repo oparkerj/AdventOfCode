db82b3c619fa8533e8ec935e5a9d36b7d49cfdee
StarShadow <oparkerj@gmail.com>
2024-03-10 16:45:45 -0700

    Remove unused methods

--- AdventToolkit.New/Parsing/ParseExtensions.cs
+++ AdventToolkit.New/Parsing/ParseExtensions.cs
@@ -10,34 +10,6 @@ namespace AdventToolkit.New.Parsing;
 public static class ParseExtensions
 {
     /// <summary>
-    /// Parse a string using the given parser.
-    /// </summary>
-    /// <param name="s">Input string.</param>
-    /// <param name="parse">Parser object.</param>
-    /// <typeparam name="T">Parse result type.</typeparam>
-    /// <typeparam name="TParse">Parser type.</typeparam>
-    /// <returns></returns>
-    public static T Parse<T, TParse>(this string s, TParse parse)
-        where TParse : ParseBase<string, T>
-    {
-        return parse.Parse(s);
-    }
-
-    /// <summary>
-    /// Parse a sequence of strings using the given parser.
-    /// </summary>
-    /// <param name="strings">Input strings.</param>
-    /// <param name="parse">Parser object.</param>
-    /// <typeparam name="T">Parse result type.</typeparam>
-    /// <typeparam name="TParse">Parser type.</typeparam>
-    /// <returns></returns>
-    public static IEnumerable<T> Parse<T, TParse>(this IEnumerable<string> strings, TParse parse)
-        where TParse : ParseBase<string, T>
-    {
-        return strings.Select(parse.Parse);
-    }
-
-    /// <summary>
     /// Parse a string using a segment parser.
     /// </summary>
     /// <param name="s">Input string.</param>
