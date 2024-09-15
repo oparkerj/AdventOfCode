f22f0174c1bade428aa6d3cddc7ffba2a3970614
StarShadow <oparkerj@gmail.com>
2024-09-14 20:07:25 -0700

    Fix the names

--- AdventToolkit2/Calc/Num.cs
+++ AdventToolkit2/Calc/Num.cs
@@ -74,16 +74,16 @@ public static class Num
     {
         return new Dictionary<int, string>
         {
-            [0] = "0",
-            [1] = "1",
-            [2] = "2",
-            [3] = "3",
-            [4] = "4",
-            [5] = "5",
-            [6] = "6",
-            [7] = "7",
-            [8] = "8",
-            [9] = "9",
+            [0] = "zero",
+            [1] = "one",
+            [2] = "two",
+            [3] = "three",
+            [4] = "four",
+            [5] = "five",
+            [6] = "six",
+            [7] = "seven",
+            [8] = "eight",
+            [9] = "nine",
         };
     }
     
